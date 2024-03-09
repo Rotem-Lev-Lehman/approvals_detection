6f3ed132b2b692ae5518334bb7e464beea8910d7
rotem <rotem@rotem-ubuntu.rotem-ubuntu>
2024-03-09 23:47:35 +0200

    reorganize question2

--- my_approvals.py
+++ my_approvals.py
@@ -1,13 +1,21 @@
 import argparse
 from web3 import Web3
-from web3.types import FilterParams
-from web3._utils.filters import construct_event_filter_params
-from web3._utils.events import get_event_data
-import json
-from web3.datastructures import AttributeDict
+from utils import (
+    get_approvals_of_owner_filter,
+    get_contract_token_symbol,
+    parse_approval_logs,
+    APPROVALS_ABI,
+)
 
-parser = argparse.ArgumentParser(description="Get all approvals for an address on the blockchain.")
-parser.add_argument("--address", type=str, required=True, help="The address on the blockchain to get approvals of.")
+parser = argparse.ArgumentParser(
+    description="Get all approvals for an address on the blockchain."
+)
+parser.add_argument(
+    "--address",
+    type=str,
+    required=True,
+    help="The address on the blockchain to get approvals of.",
+)
 
 args = parser.parse_args()
 address = int(args.address, base=0)
@@ -20,94 +28,19 @@ provider_url = f"{mainnet_url}/{api_key}"
 w3 = Web3(Web3.HTTPProvider(f"https://{provider_url}"))
 
 
+approval_contract = w3.eth.contract(abi=APPROVALS_ABI)
 
-
-# Reduced ERC-20 ABI, only Approval event
-ABI = """[
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "name": "owner",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "name": "spender",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "name": "value",
-                "type": "uint256"
-            }
-        ],
-        "name": "Approval",
-        "type": "event"
-    }
-]
-"""
-
-abi = json.loads(ABI)
-ERC20 = w3.eth.contract(abi=abi)
-
-event = ERC20.events.Approval()
+approval_event = approval_contract.events.Approval()
 codec = w3.codec
 
-data_filter_set, event_filter_params = construct_event_filter_params(
-    event.abi,
-    codec,
-    argument_filters={"owner": address},
-    fromBlock=0
+
+approvals_filter = get_approvals_of_owner_filter(
+    w3=w3, approval_event=approval_event, codec=codec, owner_address=address
 )
-
-
-approvals_filter = w3.eth.filter(event_filter_params)
 logs = approvals_filter.get_all_entries()
-
-# Convert raw binary data to Python proxy objects as described by ABI
-all_events = []
-for log in logs:
-    # Convert raw JSON-RPC log result to human readable event by using ABI data
-    # More information how process_log works here
-    # https://github.com/ethereum/web3.py/blob/fbaf1ad11b0c7fac09ba34baff2c256cffe0a148/web3/_utils/events.py#L200
-    evt = get_event_data(codec, event.abi, log)
-    # Note: This was originally yield,
-    # but deferring the timeout exception caused the throttle logic not to work
-    all_events.append(evt)
-
-print(len(all_events))
-
-
-def get_approvals_of_owner(w3: Web3, owner_address: str) -> list[AttributeDict]:
-    """
-    Returns all of the approvals that the given owner approved
-
-    Args:
-        w3 (Web3): The web3 API
-        owner_address (str): The owner address that approved
-
-    Returns:
-        list[AttributeDict]: All of the approvals that the given owner approved
-    """
-    pass
-
-
-def get_contract_token_symbol(w3: Web3, contract_address: str) -> str:
-    """
-    Returns the token's symbol of the given contract address
-
-    Args:
-        contract_address (str): The address of the contract
-
-    Returns:
-        str: The symbol of the token
-    """
-    
-    abi = [{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}]
-    
-    contract = w3.eth.contract(contract_address, abi=abi)
-
-    token_symbol = contract.functions.symbol().call() 
-    return token_symbol
+approval_events = parse_approval_logs(
+    approval_event=approval_event, codec=codec, logs=logs
+)
+for approval in approval_events:
+    token_symbol = get_contract_token_symbol(w3=w3, contract_address=approval.address)
+    print(token_symbol)
---
+++ utils.py
@@ -0,0 +1,98 @@
+from web3 import Web3
+from web3._utils.filters import construct_event_filter_params, Filter
+from web3._utils.events import get_event_data
+from web3.types import LogReceipt, EventData
+from typing import Final, Any
+from web3.contract.base_contract import BaseContractEvent
+from eth_abi.codec import ABICodec
+
+
+# Only Approval event
+APPROVALS_ABI: Final[list[dict[str, Any]]] = [
+    {
+        "anonymous": False,
+        "inputs": [
+            {"indexed": True, "name": "owner", "type": "address"},
+            {"indexed": True, "name": "spender", "type": "address"},
+            {"indexed": False, "name": "value", "type": "uint256"},
+        ],
+        "name": "Approval",
+        "type": "event",
+    }
+]
+
+# Only define the function symbol (gets the symbol of a token)
+TOKEN_SYMBOL_ABI: Final[list[dict[str, Any]]] = [
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [{"internalType": "string", "name": "", "type": "string"}],
+        "stateMutability": "view",
+        "type": "function",
+    }
+]
+
+
+def get_approvals_of_owner_filter(
+    w3: Web3,
+    approval_event: type[BaseContractEvent],
+    codec: ABICodec,
+    owner_address: str,
+) -> Filter:
+    """
+    Returns a filter of the approvals that the given owner approved
+
+    Args:
+        w3 (Web3): The web3 API
+        owner_address (str): The owner address that approved
+
+    Returns:
+        Filter: A filter of the approvals that the given owner approved
+    """
+
+    _, approval_filter_params = construct_event_filter_params(
+        approval_event.abi,
+        codec,
+        argument_filters={"owner": owner_address},
+        # Start the filter from block 0 so that all of the records will be viewed by the filter
+        fromBlock=0,
+    )
+
+    return w3.eth.filter(approval_filter_params)
+
+
+def parse_approval_logs(
+    approval_event: type[BaseContractEvent], codec: ABICodec, logs: list[LogReceipt]
+) -> list[EventData]:
+    """
+    Parse the raw binary logs to python proxy objects as described by the APPROVALS_ABI
+
+    Args:
+        logs (list[LogReceipt]): The logs to parse
+
+    Returns:
+        list[EventData]: The parsed logs
+    """
+    all_events = []
+    for log in logs:
+        event = get_event_data(codec, approval_event.abi, log)
+        all_events.append(event)
+
+    return all_events
+
+
+def get_contract_token_symbol(w3: Web3, contract_address: str) -> str:
+    """
+    Returns the token's symbol of the given contract address
+
+    Args:
+        contract_address (str): The address of the contract
+
+    Returns:
+        str: The symbol of the token
+    """
+
+    contract = w3.eth.contract(contract_address, abi=TOKEN_SYMBOL_ABI)
+
+    token_symbol = contract.functions.symbol().call()
+    return token_symbol
