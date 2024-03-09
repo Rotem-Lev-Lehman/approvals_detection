c132893cf67a713e535d7e585b9f6d729b6004f9
rotem <rotem@rotem-ubuntu.rotem-ubuntu>
2024-03-10 01:14:03 +0200

    add token price info

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ from web3.types import LogReceipt, EventData
 from typing import Final, Any
 from web3.contract.base_contract import BaseContractEvent
 from eth_abi.codec import ABICodec
+import requests
+from requests import Response
 
 
 # Only Approval event
@@ -21,16 +23,25 @@ APPROVALS_ABI: Final[list[dict[str, Any]]] = [
     }
 ]
 
-# Only define the function symbol (gets the symbol of a token)
-TOKEN_SYMBOL_ABI: Final[list[dict[str, Any]]] = [
+# Only define the symbol and name functions
+TOKEN_DATA_ABI: Final[list[dict[str, Any]]] = [
+    {
+        "inputs": [],
+        "name": "name",
+        "outputs": [{"internalType": "string", "name": "", "type": "string"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
     {
         "inputs": [],
         "name": "symbol",
         "outputs": [{"internalType": "string", "name": "", "type": "string"}],
         "stateMutability": "view",
         "type": "function",
-    }
+    },
 ]
+
+COINGECKO_URL: Final[str] = "https://api.coingecko.com/api/v3/simple/price"
 
 
 def get_approvals_of_owner_filter(
@@ -85,22 +96,25 @@ def parse_approval_logs(
     return all_events
 
 
-def get_contract_token_symbol(w3: Web3, contract_address: str) -> str:
+def get_contract_token_name_and_symbol(
+    w3: Web3, contract_address: str
+) -> tuple[str, str]:
     """
-    Returns the token's symbol of the given contract address
+    Returns the token's name and symbol of the given contract address
 
     Args:
         w3 (Web3): The web3 API
         contract_address (str): The address of the contract
 
     Returns:
-        str: The symbol of the token
+        tuple[str, str]: (name, symbol) of the given token
     """
 
-    contract = w3.eth.contract(contract_address, abi=TOKEN_SYMBOL_ABI)
+    contract = w3.eth.contract(contract_address, abi=TOKEN_DATA_ABI)
 
+    token_name = contract.functions.name().call()
     token_symbol = contract.functions.symbol().call()
-    return token_symbol
+    return token_name, token_symbol
 
 
 def print_approvals_of_owner(w3: Web3, owner_address: str):
@@ -126,7 +140,59 @@ def print_approvals_of_owner(w3: Web3, owner_address: str):
         approval_event=approval_event, codec=codec, logs=logs
     )
     for approval in parsed_approvals:
-        token_symbol = get_contract_token_symbol(
+        token_name, token_symbol = get_contract_token_name_and_symbol(
             w3=w3, contract_address=approval.address
         )
-        print(f"approval on {token_symbol} on amount of {approval.args.value}")
+        token_price = get_token_price(token_name=token_name, token_symbol=token_symbol)
+        print(
+            f"approval on {token_symbol} on amount of {approval.args.value} --- Token's price = {token_price} USD"
+        )
+
+
+def _get_token_price_from_response(response: Response, search_key: str) -> str | None:
+    """
+    Returns the token's price from the response
+
+    Args:
+        response (Response): The response of coingecko api
+        search_key (str): The key we searched by in the api
+
+    Returns:
+        str | None: The token price or None if not found
+    """
+
+    token_price = None
+    if response.status_code == 200:
+        response_data = response.json()
+        if search_key in response_data:
+            if "usd" in response_data[search_key]:
+                token_price = response_data[search_key]["usd"]
+    return token_price
+
+
+def get_token_price(token_name: str, token_symbol: str) -> str | None:
+    """
+    Returns the given token's price in USD
+
+    Args:
+        token_symbol (str): The token's symbol
+
+    Returns:
+        str | None: The token's price if found or None otherwise
+    """
+
+    token_name = token_name.lower()
+    token_symbol = token_symbol.lower()
+    search_by_name_params = {"ids": token_name, "vs_currencies": "USD"}
+    search_by_symbol_params = {"ids": token_symbol, "vs_currencies": "USD"}
+    response_by_name = requests.get(COINGECKO_URL, params=search_by_name_params)
+    token_price = _get_token_price_from_response(
+        response=response_by_name, search_key=token_name
+    )
+    if token_price:
+        return token_price
+    else:
+        response_by_symbol = requests.get(COINGECKO_URL, params=search_by_symbol_params)
+        return _get_token_price_from_response(
+            response=response_by_symbol, search_key=token_symbol
+        )
